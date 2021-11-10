a43aa12b3a5672b90403c07c58610897bd683275
pinhocesar <caesaraugustus1806@gmail.com>
2021-11-10 16:43:25 -0300

    commit inicial

---
+++ Enum/Genero.cs
@@ -0,0 +1,20 @@
+namespace DIO.Series
+{
+    public enum Genero
+    {
+        Ação = 1,
+        Aventura = 2,
+        Comédia = 3,
+        Documentário = 4,
+        Drama = 5,
+        Espionagem = 6,
+        Western = 7,
+        Fantasia = 8,
+        Scifi = 9,
+        Musical = 10,
+        Romance = 11,
+        Suspense = 12,
+        Terror = 13,
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# DIO.Series
+Aplicação console simples em C# de listagem de séries (DIO)
+
+Projeto do Bootcamp da Take Blip oferecido pela Plataforma Digital Innovation One.
